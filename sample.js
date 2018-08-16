abfd5b463417410129af0d3388691e723e155042
Rob Foulkrod <rob.foulkrod@nhls.com>
2018-08-16 15:19:48 -0400

    Afgernoon thursday

---
+++ sample.js
@@ -0,0 +1,11 @@
+javascript: (function () {
+     $(".completionStatusList:visible")
+     .filter(
+         function (index) { var value = +$(this).val();
+             var arrayPosition = [0].indexOf(value);
+              var shouldChange = arrayPosition >= 0;
+               return shouldChange;
+             })
+         .val(10).change(); })()
+
+javascript: (function(){  /* cool stuff goes here */ })();
